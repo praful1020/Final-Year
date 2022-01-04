d05b7fed8ec976ddc2761d117a959486b232cb95
Dhruvil <dhruvilkheni89@gmail.com>
2022-01-04 17:07:22 +0530

    one

--- myapp/models.py
+++ myapp/models.py
@@ -7,9 +7,8 @@ class SecUser(models.Model):
     mobile = models.CharField(max_length=10)
     password = models.CharField(max_length=20)
     pic = models.FileField(upload_to='profile',default='avatar.jpg')
+    address = models.CharField(max_length=60, null=True, blank=True)
     
-    
-
     def __str__(self):
         return self.name
       
